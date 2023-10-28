729160be9696cee3ee0cab2ce72d8ab9d2d41b73
Alex Cesar <thealexcesar@gmail.com>
2023-10-28 18:30:41 -0300

    Add Test Controller with index + show

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe Api::V1::UsersController, type: :controller do
+  before(:each) { request.headers['Accept'] = 'application/personal-api.v1' }
+
+  describe 'Get /show' do
+    before(:each) do
+      @user = FactoryBot.create(:user)
+      get :show, params: { id: @user.id }, format: :json
+    end
+
+    it 'returns info about report users' do
+      puts response.body
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eq(@user.email)
+    end
+
+    it { should respond_with :ok }
+  end
+end
